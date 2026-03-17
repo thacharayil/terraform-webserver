f1889096863de55e4dfaa2b397e4d927461fa2d2
Akhil <akhil@gmail.com>
2026-03-17 07:49:07 +0000

    security group added

--- main.tf
+++ main.tf
@@ -6,3 +6,41 @@ resource "aws_key_pair" "auth_key" {
     Name = "${var.project_name}-${var.project_environment}"
   }
 }
+
+resource "aws_security_group" "webserver" {
+
+  name        = "${var.project_name}-${var.project_environment}"
+  description = "Allow all Inbound and OutBound"
+
+  ingress {
+
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_environment}-webserver"
+  }
+}
